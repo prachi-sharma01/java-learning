382409a09c5d69cf5dd37861bf5a0642385746bc
Prachi <prachishm2408@gmail.com>
2026-04-13 23:29:12 +0530

    Added : operators in java

---
+++ ArithmeticOperators.java
@@ -0,0 +1,18 @@
+public class ArithmeticOperators {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 6;
+
+        int sum = a + b;
+        int difference = a - b;
+        int product = a * b;
+        int quotient = a / b;
+        int remainder = a % b;
+
+        System.out.println("Addition: " + sum);
+        System.out.println("Subtraction: " + difference);
+        System.out.println("Multiplication: " + product);
+        System.out.println("Division: " + quotient);
+        System.out.println("Modulus: " + remainder);
+    }
+}
